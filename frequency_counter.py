6055632a4cbbc3cd979653d4435f383740f7a869
Pete Looney <petelooney81@gmail.com>
2020-08-28 10:30:32 -0500

    cleaned up frequency_counter.py

--- frequency_counter.py
+++ frequency_counter.py
@@ -1,22 +1,23 @@
 # write a function called has_same_digit_frequency that accepts two lists and returns whether they have the same frequency of digits
 
-def has_same_digit_frequency(list1, list2):
-    dict_1 = {}
-    # assign the first lists values to a dictionary, if that value doesn't already exist as a key, then it adds it, and initiates the frequency to 1
+# function that takes a list of numbers and returns a dictionary with frequency of each number
+def create_frequency_dict(some_list):
+    dictionary = {}
+    # assigns the lists values to a dictionary, if that value doesn't already exist as a key, then it adds it, and initiates the value to 1
     # if the key already exists, then we add one to the value for that key
-    for number in list1:
-        if number not in dict_1:
-            dict_1[number] = 1
+    for number in some_list:
+        if number not in dictionary:
+            dictionary[number] = 1
         else:
-            dict_1[number] += 1
-    dict_2 = {}
-    for number in list2:
-        if number not in dict_2:
-            dict_2[number] = 1
-        else:
-            dict_2[number] += 1
+            dictionary[number] += 1
+    return dictionary
 
+def has_same_digit_frequency(list1, list2):
+    # call our create_dicitonary
+    dict_1 = create_frequency_dict(list1)
+    dict_2 = create_frequency_dict(list2)
     
+
     # first we check if then dicitonaries are the same length, if they aren't, then we return false
     if len(dict_1) != len(dict_2):
         return False
@@ -35,7 +36,7 @@ def has_same_digit_frequency(list1, list2):
         # if we can iterate through the entire dictionary without getting a return false, then they are all the same frequency and we return True
         return True
 
-first_list = [2,2,3]
+first_list = [2,3,3,4]
 second_list = [3,2,2]
 
 print(f'\n\nDo the lists {first_list} and {second_list} have the same frequency of digits?\n{has_same_digit_frequency(first_list, second_list)}\n\n')
