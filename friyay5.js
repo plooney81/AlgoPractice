941c8a23c5b412a6b22f930b904497eae9e7e7c3
Pete Looney <petelooney81@gmail.com>
2020-09-18 13:09:40 -0500

    another one

--- friyay5.js
+++ friyay5.js
@@ -2,7 +2,6 @@
 // 1).
 const someArray = [1, 2, 3];
 
-
 // function checkArray(input){
 //     return Array.isArray(input);
 // }
@@ -10,12 +9,18 @@ const someArray = [1, 2, 3];
 // console.log(checkArray(someArray));
 
 // 2). write a function to get the first n elements of an array
-function getTheNElements(stopIndex, array){
-    if(stopIndex > array.length){
+function getTheNElements(n, array){
+    if(n > array.length){
         console.log('Cant do that');
     }else{
-        return array.slice(0 , stopIndex);
+        return array.slice(0 , n);
     }
 }
 
-console.log(getTheNElements(3, someArray));
+console.log(getTheNElements(1, someArray));
+
+// 3). write a function to program to join all elements of the following array into a string.
+
+console.log(someArray.join(' '));
+
+// 4). Write a JS program which accept a number as input and insert deashes between each number
