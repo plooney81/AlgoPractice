ed3c6fd11b608b19c0b2d4c582f5c1c0eee40e7d
Pete Looney <petelooney81@gmail.com>
2020-11-28 10:01:23 -0600

    intro to problem solving - the setup

---
+++ jsAlgoDataStructuresMC/problemSolving/introToProblemSolving.js
@@ -0,0 +1,21 @@
+//! Problem Solving steps
+//* 1. Understand the problem
+//* 2. Explore concrete examples
+//* 3. Break it down
+//* 4. Solve and Simplify
+//* 5. Look back and refactor
+
+//* Understanding the problem
+//? When you get a new question you should:
+//? 1. restate the problem in your own words
+//? 2. explain what the inputs and outputs of the problem are?
+//? 3. determine if the outputs can be determined from the inputs
+//? 4. determine how the important pieces of data should be labeled for the problem
+
+//todo EXAMPLE for Understanding the problem:
+// Function that takes two #'s and returns their sum:
+// 1. Add two numbers together
+// 2. Ints, floats, string for large # ?
+// 3. Depends
+// 4. Function name (something like --> returnSum), num1, num2
+
