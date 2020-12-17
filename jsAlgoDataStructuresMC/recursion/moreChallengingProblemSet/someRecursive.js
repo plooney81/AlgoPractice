1e92afbb22e11b6a01d0efbfe0228aa129c48d93
Pete Looney <petelooney81@gmail.com>
2020-12-17 15:33:10 -0600

    recursive problem set - someRecursive refactor

--- jsAlgoDataStructuresMC/recursion/moreChallengingProblemSet/someRecursive.js
+++ jsAlgoDataStructuresMC/recursion/moreChallengingProblemSet/someRecursive.js
@@ -9,19 +9,30 @@
 // someRecursive([4, 6, 8], isOdd) //false
 // someRecursive([4, 6, 8], val => val > 10) //false
 
-const someRecursive = (arr, callback) => {
+// const someRecursive = (arr, callback) => {
     
-    let returnValue = false;
-    const helperFunction = (arr) => {
-        if(arr.length === 0) return
-        if(callback(arr[0])) returnValue = true;
+//     let returnValue = false;
+//     const helperFunction = (arr) => {
+//         if(arr.length === 0) return
+//         if(callback(arr[0])) returnValue = true;
 
-        helperFunction(arr.slice(1));
-    }
+//         helperFunction(arr.slice(1));
+//     }
 
-    helperFunction(arr);
-    return returnValue;
+//     helperFunction(arr);
+//     return returnValue;
+// }
+
+//refactor
+const someRecursive = (arr, callback) => {
+    if(arr.length === 0) return false;
+    if(callback(arr[0])) return true;
+    return someRecursive(arr.slice(1), callback);
 }
+
+
+
+
 const isOdd = val => val % 2 !== 0;
 console.log(someRecursive([1, 2, 3, 4], isOdd)); //true
 console.log(someRecursive([4, 6, 8, 9], isOdd)); //true
