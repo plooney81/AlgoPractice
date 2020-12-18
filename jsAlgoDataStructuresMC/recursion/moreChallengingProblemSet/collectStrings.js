bc8fff2fa2e047469e1ff565d683acce4d67b4bb
Pete Looney <petelooney81@gmail.com>
2020-12-18 11:59:13 -0600

    js algo & ds mc - recursion - collectStrings helper method way

--- jsAlgoDataStructuresMC/recursion/moreChallengingProblemSet/collectStrings.js
+++ jsAlgoDataStructuresMC/recursion/moreChallengingProblemSet/collectStrings.js
@@ -19,15 +19,38 @@
 
 // console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
 
+//! pure recursion way
 const collectStrings = (obj) => {
     let newArr = [];
     for(let key in obj){
+        // need to find a way to see if the value is also another object...if it is then we call the function again with the nested object as the parameter
+        // if it isn't an object, and is a string...then we push that string into our array
         if(typeof obj[key] === 'object' && obj[key] !== null){
             newArr = newArr.concat(collectStrings(obj[key]))
         }else if(typeof obj[key] === 'string'){
             newArr.push(obj[key]);
         }
     }
+
+    return newArr;
+}
+
+//! helper function way
+// more readable and intuitive in my opinion, but longer
+const collectStrings2 = (obj) => {
+    let newArr = [];
+
+    const helper = (obj) => {
+        for(let key in obj){
+            if(typeof obj[key] === 'object' && obj[key] !== null){
+                helper(obj[key]);
+            }else if(typeof obj[key] === 'string'){
+                newArr.push(obj[key]);
+            }
+        }
+    }
+
+    helper(obj);
 
     return newArr;
 }
@@ -48,4 +71,4 @@ const obj = {
     }
 }
 
-console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
+console.log(collectStrings2(obj)); // ["foo", "bar", "baz"])
