1f2afa02ad2de5406e225b063d4d943a37ef3b3a
Pete Looney <petelooney81@gmail.com>
2021-01-04 14:40:24 -0600

    dataStructures - Stacks - Push/Pop Method - Refactor

--- jsAlgoDataStructuresMC/dataStructures/stacks_queues/stacks.js
+++ jsAlgoDataStructuresMC/dataStructures/stacks_queues/stacks.js
@@ -21,6 +21,9 @@
 
 //* Linked List Implementation
 //? Notice the similarities between this and the singly/doubly linked list data structures.
+//! Alternatively, we could have done this with a singly linked list, the only difference is that we would have just done shift and unshift
+//! And called them push and pop so we could get constant time...
+//todo Because we used a doubly linked list, we are getting constant time because we can do this.last.prev instead of having to loop through the list.
 class Node {
     constructor(val){
         this.val = val;
@@ -38,7 +41,7 @@ class stack {
     //* Push Method --> Adds a new node onto the end of our stack
     //? If the stack is empty, then the newNode is first and last and has no next or previous
     //? Else, we update this.last to reflect the changes, and also change the next/previous properties of the new last and the previous last to reflect changes.
-    //? Increment the stacks size and return the stack
+    //? Increment the stacks size and return the stacks size
     push(val){
         const newNode = new Node(val);
         if(this.size === 0){
@@ -49,15 +52,14 @@ class stack {
             newNode.prev = this.last;
             this.last = newNode;
         }
-        this.size++;
-        return this;
+        return ++this.size;
     }
     //* Pop Method --> Removes the last node from the stack
-    //? If its empty we return undefined, if the size is 1, we set the first and last properties to null
+    //? If its empty we return null, if the size is 1, we set the first and last properties to null
     //? Else we make a few changes to the next and previous properties of the new last and old last properties
     //? Decrement the size and return the poppedNode
     pop(){
-        if(this.size === 0) return undefined;
+        if(this.size === 0) return null;
         const poppedNode = this.last;
         if(this.size === 1){
             this.first = null;
