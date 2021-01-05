5643e304597a74ad79fb26c373645a5ddc5690a6
Pete Looney <petelooney81@gmail.com>
2021-01-05 11:20:43 -0600

    dataStructures - Stacks - Challenge

---
+++ jsAlgoDataStructuresMC/dataStructures/stacks_queues/challenges_stacks.js
@@ -0,0 +1,82 @@
+//* Maximum Element
+//? You have an empty sequence, and you will be given N queries. Each query is one of these three types:
+//? 1. Push the element x into the stack
+//? 2. Delete the element present at the tope of the stack
+//? 3. Print the maximum element in the stack
+
+class Node{
+    constructor(data){
+        this.next = null;
+        this.prev = null;
+        this.data = data;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+        this.max = 0;
+    }
+    findMax(){
+        this.max = 0;
+        if(this.size === 0) return undefined;
+        let current = this.first;
+        while(current){
+            this.max = Math.max(this.max, current.data);
+            current = current.next;
+        }
+    }
+    push(val){
+        const newNode = new Node(val);
+        if(this.size === 0){
+            this.first = newNode;
+        }else{
+            newNode.prev = this.last;
+            this.last.next = newNode;
+        }
+        this.last = newNode;
+        this.size++;
+        this.max = Math.max(this.max, val);
+    }
+    pop(){
+        if(this.size === 0) return undefined;
+        const poppedNode = this.last;
+        if(this.size === 1){
+            this.first = null;
+            this.last = null;
+        }else{
+            this.last = poppedNode.prev;
+            this.last.next = null;
+            poppedNode.prev = null;
+        }
+        if(this.max === poppedNode.data) this.findMax();
+        this.size--;
+        return poppedNode;
+    }
+}
+
+function processData(input) {
+    let newStack = new Stack();
+    
+    input.split('\n').forEach((char, index) => {
+        if(index !== 0){
+            if(char[0] === "1"){
+                newStack.push(parseInt(char.slice(2)))
+            }else if(char[0] === '2'){
+                newStack.pop();
+            }else if(char[0] === '3'){
+                console.log(newStack.max);
+            }
+        }
+    });
+} 
+
+let newStack = new Stack();
+newStack.push(1);
+console.log(newStack);
+newStack.push(2);
+console.log(newStack);
+newStack.pop();
+console.log(newStack);
