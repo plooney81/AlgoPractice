66c552d5bd3d1c90faa5f395fef934d695739c3c
Pete Looney <petelooney81@gmail.com>
2021-01-06 15:16:47 -0600

    dataStructures - Binary Search Trees - Insert Method - Recursively

--- jsAlgoDataStructuresMC/dataStructures/binarySearchTrees/binarySearchTrees.js
+++ jsAlgoDataStructuresMC/dataStructures/binarySearchTrees/binarySearchTrees.js
@@ -18,32 +18,56 @@ class BinarySearchTree {
     //?     Else we move to the left and restart the checks
     //? If great than, we check if the root has a right --> if it doesn't then we assign newNode to the right property and return the tree
     //?     Else we move to the right and restart the checks
-    insert(val){
-        const newNode = new Node(val);
-        if(!this.root){
-            this.root = newNode;
+    // insert(val){
+    //     const newNode = new Node(val);
+    //     if(!this.root){
+    //         this.root = newNode;
+    //         return this;
+    //     }
+    //     let current = this.root;
+    //     while(current){
+    //         if(newNode.val === current.val){
+    //             current.freq += 1;
+    //             return this;
+    //         }else if(newNode.val < current.val){
+    //             if(!current.left){
+    //                 current.left = newNode;
+    //                 return this;
+    //             }
+    //             current = current.left;      
+    //         }else if(newNode.val > current.val){
+    //             if(!current.right){
+    //                 current.right = newNode;
+    //                 return this;
+    //             }
+    //             current = current.right;      
+    //         }
+    //     }
+    // }
+    //* recursively
+    insert(val, current = this.root){
+        if(!current){ 
+            this.root = new Node(val);
             return this;
-        }
-        let current = this.root;
-        while(current){
-            if(newNode.val === current.val){
-                current.freq += 1;
-                return this;
-            }else if(newNode.val < current.val){
-                if(!current.left){
-                    current.left = newNode;
-                    return this;
-                }
-                current = current.left;      
-            }else if(newNode.val > current.val){
-                if(!current.right){
-                    current.right = newNode;
-                    return this;
-                }
-                current = current.right;      
+        };
+        if(val === current.val){
+            current.freq++;
+            return this;
+        }else if(val < current.val){
+            if(current.left) return this.insert(val, current.left)
+            else{
+                current.left = new Node(val);
+            }
+        }else if(val > current.val){
+            if(current.right) return this.insert(val, current.right)
+            else{
+                current.right = new Node(val);
             }
         }
     }
+    //! Recursively
+    //* Find Method PseudoCode:
+    //? Essentially the same as the above insert...except we are saving new nodes with the value, and if we find a null left or right then we return undefined.
     find(val, current = this.root){
         if(!current) return undefined;
         if(val === current.val) return current;
@@ -59,4 +83,5 @@ tree.insert(9);
 tree.insert(8);
 tree.insert(11);
 tree.insert(10);
-console.log(tree.find(15));
+console.log(tree);
+// console.log(tree.find(15));
