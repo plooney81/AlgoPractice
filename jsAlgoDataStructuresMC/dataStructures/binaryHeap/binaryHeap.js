c4894d95229079c4857b4509b57162db5eea401a
Pete Looney <petelooney81@gmail.com>
2021-01-19 22:11:54 -0600

    DS - binaryHeaps - Intro/Insert - Recursively

--- jsAlgoDataStructuresMC/dataStructures/binaryHeap/binaryHeap.js
+++ jsAlgoDataStructuresMC/dataStructures/binaryHeap/binaryHeap.js
@@ -13,15 +13,26 @@ class MaxBinaryHeap{
         }
         return this.values;
     }
+    //* Recursively
+    insert2(val, index = 0, child = -1){
+        if(index === 0) this.values.push(val)
+        if(child === -1) child = this.values.length - 1
+        let parent = Math.floor((child - 1)/2)
+        if(this.values[child] > this.values[parent]){
+            [this.values[child], this.values[parent]] = [this.values[parent], this.values[child]];
+            return this.insert2(val, index + 1, parent)
+        }
+        return this.values
+    }
 }
 
 let newBinaryHeap = new MaxBinaryHeap();
-console.log(newBinaryHeap.insert(44));
-console.log(newBinaryHeap.insert(15));
-console.log(newBinaryHeap.insert(70));
-console.log(newBinaryHeap.insert(99));
-console.log(newBinaryHeap.insert(45));
-console.log(newBinaryHeap.insert(100));
-console.log(newBinaryHeap.insert(65));
-console.log(newBinaryHeap.insert(40));
-console.log(newBinaryHeap.insert(53));
+console.log(newBinaryHeap.insert2(44));
+console.log(newBinaryHeap.insert2(15));
+console.log(newBinaryHeap.insert2(70));
+console.log(newBinaryHeap.insert2(99));
+console.log(newBinaryHeap.insert2(45));
+console.log(newBinaryHeap.insert2(100));
+console.log(newBinaryHeap.insert2(65));
+console.log(newBinaryHeap.insert2(40));
+console.log(newBinaryHeap.insert2(53));
