d4566ef8918c89bf5a888035664b1386a1b150a4
Pete Looney <petelooney81@gmail.com>
2021-01-21 21:51:06 -0600

    DS - binaryHeaps - Priority Queue Class - Extract Max -  Python

--- jsAlgoDataStructuresMC/dataStructures/binaryHeap/priorityQueue.py
+++ jsAlgoDataStructuresMC/dataStructures/binaryHeap/priorityQueue.py
@@ -14,6 +14,12 @@ class PriorityQue:
     def swap(self, index_1, index_2):
         self.queue[index_1], self.queue[index_2] = self.queue[index_2], self.queue[index_1]
 
+    def check_index(self, index):
+        try:
+            return self.queue[index].priority
+        except IndexError:
+            return 0
+
     def insert(self, priority, val):
         self.queue.append(Node(priority, val))
         child = len(self.queue) - 1
@@ -25,6 +31,25 @@ class PriorityQue:
             else:
                 return self.queue
     
+    def extractMax(self):
+        if len(self.queue) == 1: return self.queue.pop(-1)
+        if len(self.queue) == 0: return self.queue
+        self.swap(0, -1)
+        popped = self.queue.pop(-1)
+        parent = 0
+        while True:
+            left = 2 * parent + 1
+            right = 2 * parent + 2
+            leftPriority = self.check_index(left)
+            rightPriority = self.check_index(right)
+            if (leftPriority == 0 and rightPriority == 0) or (self.queue[parent].priority > leftPriority and self.queue[parent].priority > rightPriority): return popped
+            if leftPriority >= rightPriority:
+                self.swap(parent, left)
+                parent = left
+            elif rightPriority > leftPriority:
+                self.swap(parent, right)
+                parent = right
+    
     #* Helps us print the queue a little neater
     def printQueue(self):
         for i, node in enumerate(self.queue): print(f'Position {i} - {node}')
@@ -32,7 +57,20 @@ class PriorityQue:
 pq = PriorityQue()
 pq.insert(0, 10)
 pq.insert(1, 20)
-pq.insert(1, 20)
-pq.insert(1, 20)
+pq.insert(3, 20)
+pq.insert(4, 20)
 pq.insert(10, 20)
+pq.printQueue()
+
+print(pq.extractMax())
+pq.printQueue()
+print(pq.extractMax())
+pq.printQueue()
+print(pq.extractMax())
+pq.printQueue()
+print(pq.extractMax())
+pq.printQueue()
+print(pq.extractMax())
+pq.printQueue()
+print(pq.extractMax())
 pq.printQueue()
